f5db2b6c9cf2d20e451a0e6315cd096e5ca5e41d
lmFreire <l.matheus.freire10@gmail.com>
2024-03-12 21:34:23 -0300

    Personalizando erros de validação

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,10 +2,12 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/lmfreire/primeiro_crud_go/src/configuration/rest_err"
+	"github.com/lmfreire/primeiro_crud_go/src/configuration/validation"
 	"github.com/lmfreire/primeiro_crud_go/src/controller/model/request"
+	"github.com/lmfreire/primeiro_crud_go/src/controller/model/response"
 )
 
 func CreateUser(c *gin.Context) {
@@ -13,13 +15,22 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 	
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		log.Printf("There are some incorrect fields, error=%s", err.Error())
+		
+		errRest := validation.ValidateUserError(err)
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
 
 	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID: "teste",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
+
+	c.JSON(200, response)
 
 }
